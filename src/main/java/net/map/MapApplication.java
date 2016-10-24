7078e991325267049a195017f6533645876d3aa1
imaginalis <rafbel001@utp.edu.pl>
2016-10-24 23:14:35 +0200

    First commit

---
+++ src/main/java/net/map/MapApplication.java
@@ -0,0 +1,12 @@
+package net.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+}
